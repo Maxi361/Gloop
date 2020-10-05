c6b70f95c3cb6fc547822cfa6490f7bb87143155
randomcmd <mailrandomguys@gmail.com>
2020-10-05 19:30:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import GLOOP.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new GLSchwenkkamera();
+        new GLLicht();
+
+        new GLWuerfel(0,0,0,100);
+
+    }
+}
